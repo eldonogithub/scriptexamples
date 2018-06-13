c59430339e8d644b4336dee3422779c2a350d43e
Eldon Olmstead <eldon.olmstead@rcsdev19.newpace.ca>
2018-06-13 08:39:22 -0300

    Adding updates to Makefie

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 CPPFLAGS=-O3
 
+TARGETS=main size empty segfault strings
+
 empty: empty.o a.o b.o 
 	g++ empty.o a.o b.o -o empty
 
 a.o: one.hh
 b.o: one.hh
 
-all: main size empty segfault strings
+all: $(TARGETS)
+
+.PHONY: clean
+clean: 
+	rm -f $(TARGETS)
--- one.hh
+++ one.hh
@@ -2,28 +2,29 @@
 #include <iostream>
 
 namespace X {
-template<typename A>
-void foo() {
-  std::cout << "foo(): " << typeid(A).name() << std::endl;
-} 
-      template <class Ch>
-      bool check_is_ascii(const Ch c)
-      {
-        return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
-               (c >= 0x30 && c <= 0x5B) || (c >= 0x5D && c <= 0xFF));
-      }
+  template<typename A>
+  void foo() {
+    std::cout << "foo(): " << typeid(A).name() << std::endl;
+  } 
 
-      template <>
-      bool check_is_ascii(const char c)
-      {
-        return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
-               (c >= 0x30 && c <= 0x5B) || c >= 0x5D);
-      }
+  template <class Ch>
+  bool check_is_ascii(const Ch c)
+  {
+    return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
+	   (c >= 0x30 && c <= 0x5B) || (c >= 0x5D && c <= 0xFF));
+  }
 
-      template <>
-      bool check_is_ascii(const unsigned char c)
-      {
-        return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
-               (c >= 0x30 && c <= 0x5B) || c >= 0x5D);
-      }
+  template <>
+  bool check_is_ascii(const char c)
+  {
+    return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
+	   (c >= 0x30 && c <= 0x5B) || c >= 0x5D);
+  }
+
+  template <>
+  bool check_is_ascii(const unsigned char c)
+  {
+    return (c == 0x20 || c == 0x21 || (c >= 0x23 && c <= 0x2E) ||
+	   (c >= 0x30 && c <= 0x5B) || c >= 0x5D);
+  }
 }
